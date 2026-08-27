c03f9681915b97c9571e6c0ada2515b27fc89583
Ravikumar <ravikumar.n.1988@gmail.com>
2026-08-27 18:03:36 +0530

    Integrate reusable field-error component into form generation

--- src/CrudGeneratorServiceProvider.php
+++ src/CrudGeneratorServiceProvider.php
@@ -19,6 +19,12 @@ class CrudGeneratorServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__ . '/../Stubs' => base_path('resources/vendor/crud-generator/stubs'),
             ], 'crud-generator-stubs');
+
+            // Allow publishing the component stub manually if needed
+            $this->publishes([
+                __DIR__ . '/Stubs/validation.error.stub' => resource_path('views/components/field-error.blade.php'),
+            ], 'crud-generator-components');
         }
+
     }
 }
